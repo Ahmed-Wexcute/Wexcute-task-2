912a9582607b63c6f147a07b4f94608a273a5ef7
Ahmed Amin <ahmed.amin@wexcute.com>
2023-12-04 21:33:37 +0100

    LEAR-42 change server location to access style sheets

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.use('/', express.static(path.join(__dirname)));
-
+// app.use( express.static(path.join(__dirname,"/public")));
+app.use(express.static(path.join(__dirname, '/')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../index.html'));
+  res.sendFile(path.join(__dirname, '../index.html'));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
